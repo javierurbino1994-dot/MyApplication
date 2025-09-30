884ab86a6e26c11fd11009c5eb392f2cd5eff723
Jhoan <joserx29@gmail.com>
2025-09-30 12:11:07 -0500

    Login y Registro

---
+++ app/src/main/java/com/example/myapplication/Util/Tools.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.Util
+
+class Tools {
+    class Tools {
+        companion object{
+            const val LOGTAG = "AppMyApplication"
+            const val USUARIO = "usuarios"
+        }
+    }
+}
